ebbbf2c69e237954cfae4476b7d9c08c6c51d3e8
yummygoods <melissa@yummygoods.com>
2023-02-15 19:13:44 -0500

    add spring starter stuff

---
+++ src/main/java/com/discord/FullFebruaryBackEnd/FullFebruaryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.discord.FullFebruaryBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullFebruaryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullFebruaryBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discord/FullFebruaryBackEnd/FullFebruaryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discord.FullFebruaryBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullFebruaryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
